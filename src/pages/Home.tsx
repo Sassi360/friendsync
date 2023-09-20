c136cdf2e9b7d1f5baac08c1978c9df3205fe673
Mohamed Sassi <mohamed@msassi.dev>
2023-09-20 11:54:25 -0400

    feat: setup supabase + add readme

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,32 @@
 import { Button } from "@/components/ui/button";
-import { FC } from "react";
+import { supabase } from "@/config/supabase-config";
+import { FC, useEffect, useState } from "react";
+
+interface Country {
+  name: string;
+}
 
 export const Home: FC = () => {
+  const [countries, setCountries] = useState<Country[]>([]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  async function getCountries() {
+    const { data } = await supabase.from("countries").select();
+    setCountries(data as any);
+  }
+
   return (
     <div className="h-screen flex items-center justify-center">
       <Button onClick={() => alert("Hello World")}>Welcome</Button>
+
+      <ul>
+        {countries?.map((country) => (
+          <li key={country.name}>{country.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
---
+++ vite-env.d.ts
@@ -0,0 +1,23 @@
+/// <reference types="vite/client" />
+
+interface Envs {
+    readonly VITE_SUPABASE_ANON_KEY: string;
+    readonly VITE_SUPABASE_URL: string;
+    readonly VITE_SENTRY_KEY: string;
+    readonly VITE_SENTRY_ENV: string;
+    readonly VITE_SENTRY_AUTH_TOKEN: string;
+
+    PORT: string;
+    NODE_ENV: "development" | "staging" | "production";
+    ANALYZE?: "true";
+  }
+
+  interface ImportMetaEnv extends Envs {}
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+
+  declare namespace NodeJS {
+    export interface ProcessEnv extends Envs {}
+  }
